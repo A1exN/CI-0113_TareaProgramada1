3009ac35abedca02b607ccf3272dbc9c6d12974e
AlexNavarro <alexna728@gmail.com>
2022-05-05 20:19:43 -0600

    Make actualizado con más envíos

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,34 @@
 #include "envioServicioPostal.h"
 #include "envioFedEx.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
+  vector<TipoEnvio *> envios;
+
   EnvioServicioPostal* envio1 = new EnvioServicioPostal("Segunda", 4, 632);
+  envios.push_back(envio1);
+
   EnvioFedEx* envio2 = new EnvioFedEx(11, 434);
+  envios.push_back(envio2);
 
-  cout << envio1->CalculoEnvio() << endl;
-  cout << envio2->CalculoEnvio() << endl;
+  EnvioServicioPostal* envio3 = new EnvioServicioPostal("Tercera", 13, 173);
+  envios.push_back(envio3);
 
-  delete envio1;
-  delete envio2;
+  EnvioFedEx* envio4 = new EnvioFedEx(14, 691);
+  envios.push_back(envio4);
+
+  float totalMontoEnvios {};
+
+  for (TipoEnvio* envio : envios)
+  {
+    totalMontoEnvios += envio->CalculoEnvio();
+    delete envio;
+  }
+
+  cout << "El monto total de los envíos es de " << totalMontoEnvios << std::endl;
 
 }
